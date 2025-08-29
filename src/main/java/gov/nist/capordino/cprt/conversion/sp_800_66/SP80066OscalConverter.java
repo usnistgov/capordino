e69029d0e6d57fbea95e9926e5984965facbe263
selenaxiao-nist <142448406+selenaxiao-nist@users.noreply.github.com>
2025-08-29 17:20:52 -0400

    800-66 groups, controls

---
+++ src/main/java/gov/nist/capordino/cprt/conversion/sp_800_66/SP80066OscalConverter.java
@@ -0,0 +1,148 @@
+package gov.nist.capordino.cprt.conversion.sp_800_66;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import gov.nist.capordino.cprt.conversion.AbstractOscalConverter;
+import gov.nist.capordino.cprt.conversion.InvalidFrameworkIdentifier;
+import gov.nist.capordino.cprt.pojo.CprtElement;
+import gov.nist.capordino.cprt.pojo.CprtMetadataVersion;
+import gov.nist.capordino.cprt.pojo.CprtRoot;
+import gov.nist.secauto.metaschema.model.common.datatype.markup.MarkupLine;
+import gov.nist.secauto.metaschema.model.common.datatype.markup.MarkupMultiline;
+import gov.nist.secauto.oscal.lib.model.BackMatter.Resource;
+import gov.nist.secauto.oscal.lib.model.BackMatter.Resource.Rlink;
+import gov.nist.secauto.oscal.lib.model.Catalog;
+import gov.nist.secauto.oscal.lib.model.CatalogGroup;
+import gov.nist.secauto.oscal.lib.model.Control;
+import gov.nist.secauto.oscal.lib.model.ControlPart;
+import gov.nist.secauto.oscal.lib.model.Link;
+import gov.nist.secauto.oscal.lib.model.Property;
+
+public class SP80066OscalConverter extends AbstractOscalConverter {
+    protected void assertFrameworkIdentifier() throws InvalidFrameworkIdentifier {
+        if (!cprtMetadataVersion.frameworkIdentifier.equals("SP800_66")) {
+            throw new InvalidFrameworkIdentifier("SP800_66", cprtMetadataVersion.frameworkIdentifier);
+        }
+    }
+
+    public SP80066OscalConverter(CprtMetadataVersion cprtMetadataVersion, CprtRoot cprtRoot) throws InvalidFrameworkIdentifier {
+        super(cprtMetadataVersion, cprtRoot);
+        assertFrameworkIdentifier();
+    }
+    
+    public SP80066OscalConverter(CprtMetadataVersion cprtMetadataVersion) throws IOException, InterruptedException, InvalidFrameworkIdentifier {
+        super(cprtMetadataVersion);
+        assertFrameworkIdentifier();
+    }
+
+    public SP80066OscalConverter(String frameworkVersionIdentifier) throws IOException, InterruptedException, InvalidFrameworkIdentifier {
+        super(frameworkVersionIdentifier);
+        assertFrameworkIdentifier();
+    }
+
+    private final String DESCRIPTION_ELEMENT_TYPE = "description";
+    private final String FOOTNOTE_ELEMENT_TYPE = "footnote";
+    private final String IMP_SPEC_ELEMENT_TYPE = "imp_spec";
+    private final String KEY_ACTIVITY_ELEMENT_TYPE = "key_activity";
+    private final String PUB_CROSSWALK_ELEMENT_TYPE = "pub_crosswalk";
+    private final String SAMPLE_QUESTION_ELEMENT_TYPE = "sample_question";
+    private final String SECURITY_RULE_ELEMENT_TYPE = "security_rule";
+    private final String STANDARD_ELEMENT_TYPE = "standard";
+    private final String TYPE_ELEMENT_TYPE = "type";
+
+    private final String PROJECTION_RELATIONSHIP_TYPE = "projection";
+
+    // private Map<String, Link> createdReferences = new HashMap<String, Link>();
+
+    /**
+     * The URI to use for 800-66-specific props.
+     */
+    private final URI SP_800_66_URI = URI.create("https://csrc.nist.gov/ns/SP-800-66");
+
+    /*
+     * security rule -> standard
+     * 
+     * standard -> key activity
+     * standard -> pub crosswalk
+     * 
+     * key activity -> imp spec
+     * key activity -> sample question
+     * key activity -> description
+     * 
+     * description -> description (subparts of same description)
+     * sample question -> sample question (subparts of same sample question)
+     * 
+     * imp spec -> type (of imp spec)
+     * 
+     * description -> footnote
+     * sample question -> footnote
+     * key activity -> footnote
+     * standard -> footnote
+     * 
+     */
+
+    @Override
+    protected void hydrateCatalog(Catalog catalog) {
+        catalog.setGroups(buildSecurityRuleGroups(catalog));
+    }
+
+    /**
+     * Build the top level group of the catalog, represented in CPRT as security rules.
+     */
+    private List<CatalogGroup> buildSecurityRuleGroups(Catalog catalog) {
+        // Recursively go down tree of elements, to build family groups
+        return cprtRoot.getElements().stream()
+            .filter(elem -> elem.element_type.equals(SECURITY_RULE_ELEMENT_TYPE))
+            .map(elem -> {
+                // For each 800-66 security rule, create an OSCAL group
+                CatalogGroup group = new CatalogGroup();
+                group.setId("SP_800_66_" + elem.element_identifier);
+                group.setClazz(elem.element_type);
+                group.setTitle(MarkupLine.fromMarkdown(elem.title));
+
+                group.addPart(buildPartFromElementText(elem, "overview"));
+                // For 800-66 standard, create an OSCAL control within this overall security rule group
+                group.setControls(buildStandardControls(catalog, elem.getGlobalIdentifier()));
+                
+
+
+                group.addProp(buildLabelProp(elem.title + " (" + elem.element_identifier + ")"));
+
+                return group;
+            })
+            .sorted(Comparator.comparing(CatalogGroup::getId))
+            .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+    }
+
+
+    /**
+     * Build the second level control of the catalog, represented in CPRT as standards.
+     */
+    // For 800-66 standard, create an OSCAL control
+    private List<Control> buildStandardControls(Catalog catalog, String parentId) {
+        return getRelatedElementsBySourceIdWithType(parentId, STANDARD_ELEMENT_TYPE, PROJECTION_RELATIONSHIP_TYPE).map(elem -> {
+            Control control = new Control();
+            control.setId("SP_800_66_" + elem.element_identifier);
+            control.setClazz(elem.element_type);
+            control.setTitle(createMarkupLineEscaped(elem.title));
+
+            List<ControlPart> parts = new ArrayList<ControlPart>();
+            ControlPart statementPart = buildPartFromElementText(elem, "statement");
+            // Key activity
+            // Pub crosswalk
+
+            parts.add(statementPart);
+
+            control.addProp(buildLabelProp(elem.title + " (" + elem.element_identifier + ")"));
+            
+            return control;
+        }).collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+    }
+    
+}
